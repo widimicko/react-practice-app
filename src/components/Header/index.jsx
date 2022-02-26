6ce3d895e62074415ee0dc5371a4f5bfcfe3c8d1
Micko Widi <widimicko85@gmail.com>
2022-02-26 16:58:27 +0700

    add asset in correct way

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 
+import logo from '../../assets/logo192.png'
+
 function Header() {
  return (
    <header className="p-3 border-b-1 shadow">
     <nav className="flex justify-between">
-      <img src="logo192.png" className="w-[40px]" alt="" />
+      <img src={logo} className="w-[40px]" alt="" />
       <ul className="flex gap-5">
         <li>Pricing</li>
         <li>About</li>
