f02b5b3fc22b6f1cd9eb97ed7772cb08c37543b9
Micko Widi <widimicko85@gmail.com>
2022-02-28 15:54:23 +0700

    add dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+
 import Info from './components/Info'
 import About from './components/About'
 import Interests from "./components/Interests";
 import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false)
+
+  function toggleDarkMode() {
+    setDarkMode(prevTheme => !prevTheme)
+  }
+
   return (
-    <div className="flex justify-center">
-      <div className="w-[400px] bg-[#1A1B21] rounded-t-3xl">
-        <Info />
-        <div className="p-4 text-white text-justify">
-          <About />
-          <Interests />
+    <div className={darkMode ? 'dark' : ''}>
+      <Navbar 
+        darkMode={darkMode}
+        toggleDarkMode={toggleDarkMode}
+      />
+      <div className="flex justify-center">
+        <div className="w-[400px] dark:bg-[#1A1B21] rounded-t-3xl border shadow-lg">
+          <Info />
+          <div className="p-4 dark:text-white text-justify">
+            <About />
+            <Interests />
+          </div>
+          <Footer />
         </div>
-        <Footer />
       </div>
     </div>
   );
--- src/components/Info/index.jsx
+++ src/components/Info/index.jsx
@@ -7,13 +7,13 @@ function Info() {
    <div>
     <img src={image} alt="" className="w-full" />
     <div className="text-center mb-4">
-      <p className="text-white font-bold text-2xl">Laura Smith</p>
+      <p className="dark:text-white font-bold text-2xl">Laura Smith</p>
       <p className="text-orange-500">Frontend Developer</p>
-      <p className="text-white">laurasmith.website</p>
+      <p className="dark:text-white">laurasmith.website</p>
     </div>
     <div className="flex justify-center gap-7 mb-5">
-      <button className="bg-white text-black rounded-lg w-[115px] h-[34px]">Email</button>
-      <button className="bg-blue-400 text-white rounded-lg text-center w-[115px] h-[34px]"> Linkedin</button>
+      <button className="dark:bg-white dark:text-black rounded-lg w-[115px] h-[34px] border dark:border-0">Email</button>
+      <button className="bg-blue-400 text-white rounded-lg text-center w-[115px] h-[34px] border dark:border-0"> Linkedin</button>
     </div>
    </div>
  )
